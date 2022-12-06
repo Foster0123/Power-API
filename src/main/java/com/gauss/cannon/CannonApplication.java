76e589ffd7fbcbf5995fe35432abab444663a039
Foster <72973015+Foster0123@users.noreply.github.com>
2022-12-06 14:58:03 +0530

    SpringBootApp Commit 1st

---
+++ src/main/java/com/gauss/cannon/CannonApplication.java
@@ -0,0 +1,13 @@
+package com.gauss.cannon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CannonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CannonApplication.class, args);
+	}
+
+}
