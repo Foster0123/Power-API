38dca13621a704083b424e9b314b545bc0cce944
Foster Z <fosterunlimited0123@gmail.com>
2022-12-07 15:52:52 +0000

    2nd SpringBootApp-Java Commit

--- src/main/java/com/gauss/cannon/CannonApplication.java
+++ src/main/java/com/gauss/cannon/CannonApplication.java
@@ -2,12 +2,23 @@ package com.gauss.cannon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class CannonApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CannonApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public String index() {
+		return "Hello From Spring-Boot-App-Java";
+	}
+	@GetMapping("/about")
+	public String about(@RequestParam(value= "name", defaultValue = "foss") String name){
+		return String.format("Showing About Page %s", name);
+	}
 }
